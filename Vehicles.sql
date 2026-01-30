cfc0f8d72d05ff60e6bb7792293de1c4a421cb97
s-maina <shatra.maina@strathmore.edu>
2026-01-31 01:51:44 +0300

    Adding rentals and vehicles

--- Vehicles.sql
+++ Vehicles.sql
@@ -17,3 +17,23 @@ CREATE TABLE locations (
   zip_code varchar(10) NOT NULL,
   region varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `rentals` (
+  `rental_id` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `total_cost` decimal(10,2) NOT NULL,
+  `rental_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `vehicles` (
+  `vehicle_id` int(11) NOT NULL,
+  `make` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `year` int(11) NOT NULL,
+  `vehicle_type` varchar(20) NOT NULL,
+  `daily_rate` decimal(10,2) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
